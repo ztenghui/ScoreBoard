7ef3a812cf5036d65c797903719cc7099ec9d2e9
localhost <localhost@ztenghui-macbookpro.local>
2011-01-14 15:54:41 -0800

    Email sort of work now

--- src/org/svcba/scoreboard/dialog/StatActivity.java
+++ src/org/svcba/scoreboard/dialog/StatActivity.java
@@ -14,19 +14,27 @@ import org.svcba.scoreboard.model.Team;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class StatActivity extends Activity
 {
 	public static final int HOME = 0;
 	public static final int AWAY = 1;
+	public String currentStats;
 	
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		
+		
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 
 		setContentView(R.layout.dlg_stat);
@@ -37,7 +45,7 @@ public class StatActivity extends Activity
 		SimpleAdapter sa = new SimpleAdapter(this, getStat(side),R.layout.stat_row,new String[]{"name","1p", "2p", "3p", "p", "rb","ass","stl", "f", "to"},new int[]{R.id.stat_name,R.id.stat_1p,R.id.stat_2p,R.id.stat_3p,R.id.stat_p,R.id.stat_rb,R.id.stat_ass,R.id.stat_stl,R.id.stat_f,R.id.stat_to});
 		lv.setAdapter(sa);
 				
-}
+    }
 	
 	private List<Map<String, Object>> getStat(int side)
 	{
@@ -144,6 +152,20 @@ public class StatActivity extends Activity
 				default:
 			}
 		}
+		currentStats = new String();
+		//Title line
+		Resources res = getResources();
+		currentStats += res.getString(R.string.ab_name)+",\t";
+		currentStats += res.getString(R.string.ab_1p)+",\t";
+		currentStats += res.getString(R.string.ab_2p)+",\t";
+		currentStats += res.getString(R.string.ab_3p)+",\t";
+		currentStats += res.getString(R.string.ab_p)+",\t";
+		currentStats += res.getString(R.string.ab_rb)+",\t";
+		currentStats += res.getString(R.string.ab_f)+",\t";
+		currentStats += res.getString(R.string.ab_to)+",\t";
+		currentStats += res.getString(R.string.ab_ass)+",\t";
+		currentStats += res.getString(R.string.ab_steal)+",\n";
+		
 		for (PlayerStat p : players.values())
 		{
 			Map<String, Object> player = new HashMap<String, Object>();
@@ -158,21 +180,44 @@ public class StatActivity extends Activity
 			player.put("ass", p.getAssist());
 			player.put("stl", p.getSteal());
 			result.add(player);
+			
+			currentStats+=(p.getName() + ",\t");
+			currentStats+=(p.get1P() + ",\t");
+			currentStats+=(p.get2P() + ",\t");
+			currentStats+=(p.get3P() + ",\t");
+			currentStats+=(p.getP() + ",\t");
+			currentStats+=(p.getRB() + ",\t");
+			currentStats+=(p.getF() + ",\t");
+			currentStats+=(p.getTO() + ",\t");
+			currentStats+=(p.getAssist() + ",\t");
+			
+			currentStats+=(p.getSteal() + ",\n");
+			
 		}
+		
+		return result;
+	}
+	
+	private String getDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+	
+	public void onBackPressed()
+	{    
+	    
 		final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);		 
         emailIntent.setType("plain/text");
         emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"ztenghui@gmail.com"});
 
-        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "mySubject");
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Game Result " + getDateTime());
 
-        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "myBodyText");
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, currentStats);
 
         startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-		return result;
-	}
-	public void onBackPressed()
-	{
-		finish();
+		
+        finish();
 		super.onBackPressed();
 	}
 }
