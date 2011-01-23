09438a0bbfe4f2426cd2f23808fb8f885a30a3c1
localhost <localhost@ztenghui-macbookpro.local>
2011-01-23 15:40:25 -0800

    fix one crash caused by delete at no action

--- src/org/svcba/scoreboard/dialog/ActionRemoveActivity.java
+++ src/org/svcba/scoreboard/dialog/ActionRemoveActivity.java
@@ -22,65 +22,77 @@ public class ActionRemoveActivity extends Activity {
 	public static final int RESULT_AWAYTEAM = 1;
 
 	public void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.dlg_action_remove);
-		
 		// Update the info
 		int pos2Remove = getIntent().getIntExtra("pos2Remove", 0);
 		Game game = ((SVCBAApp)getApplicationContext()).getGame();
 		List<Action> action = game.getActions();
-		Action displayAction = action.get(action.size()-1-pos2Remove);
+		Action displayAction = null;
 		
-		// should merge the code with NormalActivity::updateAction()
+		boolean notImplemnted = true;
 		String realAction = new String();
 		Resources res = getResources();
-		boolean notImplemnted = false;
-		switch (displayAction.getAction())
+		if (action.size()-1-pos2Remove>=0 && action.size()>0)
 		{
-			case Action.TIMEOUT:				
-				realAction = res.getString(R.string.notimplemented);
-				notImplemnted = true;
-				break;
-			case Action.SUB_OFF:				
-				realAction = res.getString(R.string.notimplemented);
-				notImplemnted = true;
-				break;
-			case Action.SUB_ON:				
-				realAction = res.getString(R.string.notimplemented);
-				notImplemnted = true;
-				break;
-			case Action.FOUL:				
-				realAction = res.getString(R.string.foul);				
-				break;
-			case Action.TURNOVER:				
-				realAction = res.getString(R.string.turnover);
-				break;
-			case Action.SHOOT_1:
-				realAction = res.getString(R.string.shoot1);				
-				break;
-			case Action.SHOOT_2:				
-				realAction = res.getString(R.string.shoot2);				
-				break;
-			case Action.SHOOT_3:				
-				realAction = res.getString(R.string.shoot3);
-				break;
-			case Action.REBOUND_B:				
-				realAction = res.getString(R.string.rebound_b);				
-				break;
-			case Action.REBOUND_F:				
-				realAction = res.getString(R.string.rebound_f);				
-				break;
-			case Action.STEAL:				
-				realAction = res.getString(R.string.steal);				
-				break;
-			case Action.ASSIST:				
-				realAction = res.getString(R.string.assist);				
-				break;
-			default:
-				break;
-		}
+			displayAction = action.get(action.size()-1-pos2Remove);
+			notImplemnted = false;
 		
+			// should merge the code with NormalActivity::updateAction()
+			
+			
+			
+			switch (displayAction.getAction())
+			{
+				case Action.TIMEOUT:				
+					realAction = res.getString(R.string.notimplemented);
+					notImplemnted = true;
+					break;
+				case Action.SUB_OFF:				
+					realAction = res.getString(R.string.notimplemented);
+					notImplemnted = true;
+					break;
+				case Action.SUB_ON:				
+					realAction = res.getString(R.string.notimplemented);
+					notImplemnted = true;
+					break;
+				case Action.FOUL:				
+					realAction = res.getString(R.string.foul);				
+					break;
+				case Action.TURNOVER:				
+					realAction = res.getString(R.string.turnover);
+					break;
+				case Action.SHOOT_1:
+					realAction = res.getString(R.string.shoot1);				
+					break;
+				case Action.SHOOT_2:				
+					realAction = res.getString(R.string.shoot2);				
+					break;
+				case Action.SHOOT_3:				
+					realAction = res.getString(R.string.shoot3);
+					break;
+				case Action.REBOUND_B:				
+					realAction = res.getString(R.string.rebound_b);				
+					break;
+				case Action.REBOUND_F:				
+					realAction = res.getString(R.string.rebound_f);				
+					break;
+				case Action.STEAL:				
+					realAction = res.getString(R.string.steal);				
+					break;
+				case Action.ASSIST:				
+					realAction = res.getString(R.string.assist);				
+					break;
+				default:
+					break;
+			}
+		}
+		else
+		{
+			realAction = res.getString(R.string.invalideitem);
+		}
 		TextView textAction = (TextView) findViewById(R.id.action_to_remove);
 		if (!notImplemnted)
 		{
